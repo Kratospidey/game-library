23def0c32d5b4b79b85ac55dbcd935c133e6e8d9
kratospidey <67730931+Kratospidey@users.noreply.github.com>
2023-06-21 16:44:26 +0530

    create ListGroup Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+	let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+	const handleSelectItem = (item: string) => {
+		console.log(item);
+	};
+
 	return (
 		<div>
-			<Message />
+			<ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
 		</div>
 	);
 }
