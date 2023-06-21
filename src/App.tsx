ec4252616f345522174b45620b0cd46b8b41d401
kratospidey <67730931+Kratospidey@users.noreply.github.com>
2023-06-21 17:20:35 +0530

    Create Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // import ListGroup from "./components/ListGroup";
 
-import Alert from "./components/Alert";
+// import Alert from "./components/Alert";
+
+import Button from "./components/Button";
 
 function App() {
 	// let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -12,9 +14,16 @@ function App() {
 	return (
 		<div>
 			{/* <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /> */}
-			<Alert>
+			{/* <Alert>
 				Hello <span>World!</span>
-			</Alert>
+			</Alert> */}
+			<Button
+				onClick={() => {
+					console.log("Clicked");
+				}}
+			>
+				Hello
+			</Button>
 		</div>
 	);
 }
